4b4e5afe449a59d54942dae6dcd202925efe7737
Suru <garudahorror25@gmail.com>
2024-09-06 10:37:08 -0400

    <message> deleted some comments

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -1,5 +1,3 @@
-//The Fibonacci Sequence, is a numerical sequence where each number is the sum of the two numbers before it. 
-//Eg. 0, 1, 1, 2, 3, 5, 8, 13 are the first eight digits in the sequence.
 const sequence = 8;
 
 function fibs(num){
--- merge_sort/index.js
+++ merge_sort/index.js
@@ -1,12 +1,10 @@
 const test1 = [3, 2, 1, 13, 8, 5, 0, 1]; //0, 1, 1, 2, 3, 5, 8, 13
-const test2 = [105, 79, 100, 110];
-const test3 = [69];
-const test4 = [3, 2, 1, 13, 25, 8, 5, 0, 1];
+const test2 = [105, 79, 100, 110]; //79, 100, 105, 110
+const test3 = [69]; //69
+const test4 = [3, 2, 1, 13, 25, 8, 5, 0, 1]; //0, 1, 1, 2, 3, 5, 8, 13, 25
 
 function mergeSort(array){
   if(array.length === 1) return [array];
-  //First step: i make an array of single arrays [[2][5][3]] [[1][6][8]]
-  //Second step: Organized the elements in both arrays into a single array [1, 2, 3, 5, 6, 8]
   const left = mergeSort(array.slice(0, Math.floor(array.length/2)));
   const right = mergeSort(array.slice(Math.floor(array.length/2)));
 
@@ -29,7 +27,7 @@ function mergeSort(array){
   return sortedArr;
 }
 
-console.log(`Test 1: ${mergeSort(test1)}`);
-console.log(`Test 2: ${mergeSort(test2)}`);
-console.log(`Test 3: ${mergeSort(test3)}`);
-console.log(`Test 4: ${mergeSort(test4)}`);
+console.log(mergeSort(test1));
+console.log(mergeSort(test2));
+console.log(mergeSort(test3));
+console.log(mergeSort(test4));
